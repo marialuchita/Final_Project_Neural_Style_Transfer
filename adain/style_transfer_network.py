724d8859c0902b5df8abf1fda7c773baec2f6d91
marialuchita <maria.luchita@protonmail.com>
2026-01-19 17:25:36 +0100

    train started

--- adain/style_transfer_network.py
+++ adain/style_transfer_network.py
@@ -2,8 +2,9 @@ import torch
 import torch.nn as nn
 from torchvision import models
 
-from typing import Union, List
+from typing import Union, List, Tuple
 
+# Fixed
 class VGGEncoder(nn.Module):
     def __init__(self):
         super().__init__()
@@ -23,7 +24,7 @@ class VGGEncoder(nn.Module):
 
     def forward(self, x: torch.Tensor, return_all: bool = False) -> Union[torch.Tensor, List[torch.Tensor]]:
         """
-        :param t: Tensor of shape (N, 3, H, W)
+        :param x: Tensor of shape (N, 3, H, W)
         :return: Union[torch.Tensor, List[torch.Tensor]]
         """
         relu1_1_features = self.group1(x)
@@ -35,8 +36,7 @@ class VGGEncoder(nn.Module):
             return [relu1_1_features, relu2_1_features, relu3_1_features, relu4_1_features]
         return relu4_1_features
 
-
-
+# Trained
 class VGGDecoder(nn.Module):
     def __init__(self):
         super().__init__()
@@ -79,3 +79,47 @@ class VGGDecoder(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.decoder(x)
 
+# Fixed
+class AdaINLayer(nn.Module):
+    def __init__(self, encoder: VGGEncoder, decoder: VGGDecoder):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, content_img: torch.Tensor, style_img: torch.Tensor, alpha: float = 1.0) -> torch.Tensor:
+        """
+
+        :param content_img: ImageNet-normalized content image. Dimensions: (N, 3, H, W)
+        :param style_img:  ImageNet-normalized style image. Dimensions: (N, 3, H, W)
+        :param alpha: Style interpolation weight from 0 to 1
+        :return: Stylized image. Dimensions: (N, 3, H, W)
+        """
+
+        content_features = self.encoder(content_img)
+        style_features = self.encoder(style_img)
+
+        target_f = AdaINLayer.adain(content_features, style_features)
+
+        # Style - content interpolation
+        target_f = alpha * target_f + (1 - alpha) * content_features
+
+        return self.decoder(target_f)
+
+
+    @staticmethod
+    def calc_statistics(features: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        epsilon = 1e-5
+        batch_size, channels, _, _ = features.shape
+        f = features.view(batch_size, channels, -1)
+        mean = f.mean(dim=2).view(batch_size, channels, 1, 1)
+        variance = f.var(dim=2, unbiased=False).view(batch_size, channels, 1, 1)
+        std = (epsilon + variance).sqrt()
+        return mean, std
+
+    @staticmethod
+    def adain(content_features: torch.Tensor, style_features: torch.Tensor) -> torch.Tensor:
+
+        content_mean, content_std = AdaINLayer.calc_statistics(content_features)
+        style_mean, style_std = AdaINLayer.calc_statistics(style_features)
+        target_features = style_std * (content_features - content_mean) / content_std + style_mean
+        return target_features
